b5f70d3ce86db6f58edfcb18424389d9c5692900
moha-boukhatem <moha94orly@gmail.com>
2021-05-05 23:19:00 +0100

    add CRUD Requests

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/v1/index');
+var usersRouter = require('./routes/v1/users');
 
 var app = express();
 
